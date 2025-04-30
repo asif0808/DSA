4fac3f3ebd09e79ce1b455c2b09226cee0dac0f9
Aasif Ansari <116907759+asif0808@users.noreply.github.com>
2025-04-30 11:59:19 +0530

    Delete React directory

--- React/01react/src/App.js
+++
@@ -1,9 +0,0 @@
-
-
-function App() {
-  return (
-   <h1>Hello from basic react</h1>
-  );
-}
-
-export default App;
--- React/01vitereact/src/App.jsx
+++
@@ -1,9 +0,0 @@
-
-function App() {
-  return (
-    
-  <h1>Hello there. 01 vite</h1>
-  )
-}
-
-export default App
